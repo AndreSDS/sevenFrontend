57f0f27d2a53d744e02cdea48e7f97055a930e53
andresds <andre_smiths@outlook.com>
2020-04-23 22:16:56 -0300

    começando novamente

--- backend/appbackend/api/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class User(models.Model):
-    nome = models.CharField(max_length=20)
-    sobreNome = models.CharField(max_length=32)
-    senha = models.CharField(max_length=10)
--- backend/appbackend/api/serializers.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User
-from rest_framework import serializers
-from .models import User
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['nome', 'sobreNome', 'senha']
